b6ade6caf05a385732046f432900e2c0bf5badad
donny <gamespike2022@gmail.com>
2023-01-06 14:05:38 +0800

    fix: update create table sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -6,26 +6,26 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order` (
-  `order_id` varchar(255) NOT NULL,
-  `uuid` varchar(255) DEFAULT NULL,
-  `from` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `to` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `contract_address` varchar(255) NOT NULL,
-  `tx_hash` varchar(255) DEFAULT NULL,
-  `status` tinyint DEFAULT '0',
-  `notify_status` tinyint DEFAULT '0',
-  `create_time` bigint DEFAULT NULL,
-  `pay_time` bigint DEFAULT NULL,
-  `amount` varchar(255) DEFAULT NULL,
-  `token_id` bigint DEFAULT NULL,
-  `cb` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    `order_id` varchar(255) NOT NULL,
+    `uuid` varchar(255) DEFAULT NULL,
+    `from` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    `to` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    `contract_address` varchar(255) NOT NULL,
+    `tx_hash` varchar(255) DEFAULT NULL,
+    `status` tinyint DEFAULT '0',
+    `notify_status` tinyint DEFAULT '0',
+    `create_time` bigint DEFAULT NULL,
+    `pay_time` bigint DEFAULT NULL,
+    `amount` varchar(255) DEFAULT NULL,
+    `token_id` bigint DEFAULT NULL,
+    `cb` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
 
-SET NAMES utf8mb4;
+SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
 
@@ -34,13 +34,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `nft_owner`;
 CREATE TABLE `nft_owner` (
-    `id` varchar(200) NOT NULL,
-    `owner_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `id` varchar(255) NOT NULL,
+    `owner_address` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
     `contract_address` varchar(255) NOT NULL,
     `token_id` bigint NOT NULL,
     `update_time` bigint NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
@@ -54,8 +54,8 @@ DROP TABLE IF EXISTS `api_key`;
 CREATE TABLE `api_key` (
     `id` varchar(255) NOT NULL,
     `api_key` varchar(255) NOT NULL,
-     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
