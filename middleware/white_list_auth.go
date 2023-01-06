f359f5458ffe2e39dba4d2ad9d0f9e4912a9f4d2
donny <gamespike2022@gmail.com>
2023-01-06 14:06:31 +0800

    feat: add getRealIp func

--- middleware/white_list_auth.go
+++ middleware/white_list_auth.go
@@ -4,12 +4,18 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spike-engine/spike-web3-server/config"
 	"github.com/spike-engine/spike-web3-server/response"
+	"github.com/spike-engine/spike-web3-server/util"
 )
 
 func WhiteListAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		clientIp := c.ClientIP()
+		clientIp := util.GetRealIp(c.Request)
 		log.Infof("clientIp : %s", clientIp)
+		if util.IsLocalIp(clientIp) {
+			c.Next()
+			return
+		}
+
 		for _, ip := range config.Cfg.TxApiWhiteList.IpList {
 			if ip == clientIp {
 				c.Next()
--- util/ip.go
+++ util/ip.go
@@ -1,1 +1,77 @@
 package util
+
+import (
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func ClientIp(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+
+	ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr))
+	if err == nil {
+		log.Infof("-----")
+		return ip
+	}
+	return ""
+}
+
+func ClientPublicIP(r *http.Request) string {
+	var ip string
+	for _, ip = range strings.Split(r.Header.Get("X-Forwarded-For"), ",") {
+		ip = strings.TrimSpace(ip)
+		if ip != "" && !IsLocalIp(ip) {
+			return ip
+		}
+	}
+
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" && !IsLocalIp(ip) {
+		return ip
+	}
+
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		log.Infof("-----")
+		if !IsLocalIp(ip) {
+			return ip
+		}
+	}
+	return ""
+}
+
+func IsLocalIp(ip string) bool {
+	if ip == "::1" {
+		return true
+	}
+	ipAddr := strings.Split(ip, ".")
+	if strings.EqualFold(ipAddr[0], "10") {
+		return true
+	} else if strings.EqualFold(ipAddr[0], "172") {
+		addr, _ := strconv.Atoi(ipAddr[1])
+		if addr >= 16 && addr < 31 {
+			return true
+		}
+	} else if strings.EqualFold(ipAddr[0], "192") && strings.EqualFold(ipAddr[1], "168") {
+		return true
+	}
+	return false
+}
+
+func GetRealIp(r *http.Request) string {
+	ip := ClientPublicIP(r)
+	if ip == "" {
+		ip = ClientIp(r)
+	}
+	return ip
+}
